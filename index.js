1f9f6a6e82a2566d5c81c71f108982a2caf79685
Dodeedoo <86907843+Dodeedoo@users.noreply.github.com>
2021-07-04 02:00:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const mineflayer = require('mineflayer');
+const client = new Discord.Client();
+
+let prefix = ".";
+let bot = mineflayer.createBot({
+    version: "1.16.5",
+    host: "minehut.com",
+    username: "ianngrover@gmail.com",
+    password: "Ing10142002!"
+})
+
+client.on("ready", async => {
+    console.log('discord bot online!')
+})
+
+bot.on("login", async => {
+    console.log('minecraft bot online!')
+})
+
+
+bot.on('chat', (username, message) => {
+    if (message.includes('join')) {
+        let channel = client.channels.cache.get('861036037089394688')
+        channel.send(`${username}: ${message}`)
+    }
+  })
+
+bot.on('kicked', (reason, loggedin) => {
+    let bot = mineflayer.createBot({
+        version: "1.16.5",
+        host: "minehut.com",
+        username: "ianngrover@gmail.com",
+        password: "Ing10142002!"
+    })
+    console.log('kicked! rejoining...')
+})
+
+client.login('ODYxMDM3Nzg4NTY0MTYwNTUy.YOD97g.j796Yv1KeEVMG1aR53QJS9sR6N0') 
